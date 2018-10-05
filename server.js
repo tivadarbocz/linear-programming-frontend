abf67596ba2f621591b0494f8f4606318cd58e05
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2018-10-05 21:13:57 +0200

    init

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/linear-programming-frontend'));
+app.use(express.static(__dirname + '/dist/linear-programming'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/linear-programming-frontend\'/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/linear-programming\'/index.html'));
 });
 
 // default Heroku port
